6ebf4f020dca95516c45ee12bb7a6acb0fc46e0e
songxvdong <songxudong@kongkonghu.com>
2016-08-05 19:41:32 +0800

    eblog页面提交

---
+++ config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by songxvdong on 2016/8/2.
+ */
+"use strict";
+module.exports = {
+    cookieSecret: 'eblog',
+    db: 'eblog',
+    host: 'localhost',
+    port: 27017
+};
---
+++ models/db.js
@@ -0,0 +1,10 @@
+/**
+ * Created by songxvdong on 2016/8/2.
+ */
+"use strict";
+
+var config = require('../config'),
+    Db = require('mongodb').Db,
+    Connection = require('mongodb').Connection,
+    Server = require('mongodb').Server;
+module.exports = new Db(config.db, new Server(config.host, config.port), {safe: true});
